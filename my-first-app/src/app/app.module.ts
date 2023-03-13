aa9452e2e69f657f815efa3e2dada2c86936e5d8
ThheKNG <kkarundu8@gmail.com>
2023-03-13 09:35:07 +0000

    Final

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ComponentaComponent } from './componenta/componenta.component';
-import { ComponentbComponent } from './componentb/componentb.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ComponentaComponent,
-    ComponentbComponent
   ],
   imports: [
     BrowserModule,
